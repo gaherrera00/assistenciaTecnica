258f9f7a9a45b1e167664909296537d286a43714
gxstavoo <157588674+russ1anyk@users.noreply.github.com>
2025-07-31 14:52:43 -0300

    alterações na estrutura de pastas

---
+++ meu-projeto/app/components/navbar/page.jsx
@@ -0,0 +1,16 @@
+export default function Header () {
+    return ( <>
+    <header>
+        <nav>
+            <div className="header">
+                <ul>
+                    <li>Home</li>
+                    <li>Sobre</li>
+                    <li>Cadastro</li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    
+    </>
+)};
