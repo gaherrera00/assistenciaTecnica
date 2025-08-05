9e7448c922709d29cc90877c61619da635221abf
gaherrera00 <gabriel.h.demarchi@gmail.com>
2025-08-05 14:27:25 -0300

    resolvi dnv

--- meu-projeto/app/components/navbar/page.jsx
+++ meu-projeto/app/components/navbar/page.jsx
@@ -1,21 +1,28 @@
 export default function Header() {
-    return (
-      <header className="bg-[#084438] text-white shadow-lg">
-        <nav className="container mx-auto px-6 py-4 flex items-center justify-between">
-          <div className="text-2xl font-bold tracking-wide">MeuSite</div>
-          <ul className="flex space-x-8 text-base font-medium">
-            <li className="hover:text-green-200 transition-colors duration-300 cursor-pointer text-white">
+  return (
+    <header className="bg-[#084438] text-white shadow-lg">
+      <nav className="container mx-auto px-6 py-4 flex items-center justify-between">
+        <div className="text-2xl font-bold tracking-wide text-white">
+          MeuSite
+        </div>
+        <ul className="flex space-x-8 text-base font-medium">
+          <li>
+            <span className="hover:text-[#1b806b] transition-colors duration-300 cursor-pointer text-white block">
               Home
-            </li>
-            <li className="hover:text-green-200 transition-colors duration-300 cursor-pointer text-white">
+            </span>
+          </li>
+          <li>
+            <span className="hover:text-[#1b806b] transition-colors duration-300 cursor-pointer text-white block">
               Sobre
-            </li>
-            <li className="hover:text-green-200 transition-colors duration-300 cursor-pointer text-white">
+            </span>
+          </li>
+          <li>
+            <span className="hover:text-[#1b806b] transition-colors duration-300 cursor-pointer text-white block">
               Cadastro
-            </li>
-          </ul>
-        </nav>
-      </header>
-    );
-  }
-  
+            </span>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
